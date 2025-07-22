e765f185cfffb83289ee8b998ef520bf25da6f3f
Alok Sahoo <admin@panchsofttechnologies.com>
2025-07-22 11:22:47 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // ✅ THIS disables ESLint during build
+    ignoreDuringBuilds: true,
   },
 };
 
